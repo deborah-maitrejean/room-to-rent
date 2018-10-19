00d192254c9f768e6722ede4e0e5ab77c771be6a
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-10-19 22:40:34 +0200

    create HomeController and home template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+#use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
