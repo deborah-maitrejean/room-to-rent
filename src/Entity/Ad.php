832177be705b0bbb9088bf5a68294ce6d8d60ff8
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-10-21 22:38:42 +0200

    create and edit an ad, collection type, js

--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -6,10 +6,16 @@ use Cocur\Slugify\Slugify;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AdRepository")
  * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity(
+ *     fields={"title"},
+ *     message="Une autre annonce possède déjà ce titre."
+ * )
  */
 class Ad
 {
@@ -22,6 +28,9 @@ class Ad
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=10, max=255, minMessage="Le titre doit faire plus de 10 caractères",
+     *     maxMessage="Le titre ne doit pas faire plus de 255 caractères
+     * ")
      */
     private $title;
 
@@ -37,16 +46,19 @@ class Ad
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\Length(min=10, minMessage="L'introduction doit faire plus de 10 caractères")
      */
     private $introduction;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\Length(min=100, minMessage="Le contenu doit faire plus de 100 caractères")
      */
     private $content;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Url()
      */
     private $coverImage;
 
@@ -57,6 +69,7 @@ class Ad
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Image", mappedBy="ad", orphanRemoval=true)
+     * @Assert\Valid
      */
     private $images;
 
