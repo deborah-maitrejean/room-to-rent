cb306bac875fd1bf068064ce1c41428dfbceed67
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-10-28 17:40:53 +0100

    create admin dashboard, use DQL and services

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 #use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +13,15 @@ class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
+     * @param AdRepository $adRepo
+     * @param UserRepository $userRepo
+     * @return Response
      */
-    public function home()
+    public function home(AdRepository $adRepo, UserRepository $userRepo)
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'ads' => $adRepo->findBestAds(3),
+            'users' => $userRepo->findBestUsers(2),
         ]);
     }
 }
